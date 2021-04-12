b355445d9c05618254d74886572bab760d31c616
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-04-12 17:05:28 -0300

    Database seed

--- backend/src/main/java/com/lucasmonteiro/movieflix/entities/Review.java
+++ backend/src/main/java/com/lucasmonteiro/movieflix/entities/Review.java
@@ -2,7 +2,9 @@ package com.lucasmonteiro.movieflix.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -19,13 +21,15 @@ public class Review implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(columnDefinition = "TEXT")
 	private String text;
 
 	@ManyToOne
 	@JoinColumn(name = "user_id")
 	private User user;
 	
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.EAGER)
 	@JoinColumn(name = "movie_id")
 	private Movie movie;
 	
