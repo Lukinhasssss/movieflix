0a755e0a8de48b96f6cc4fe849532ae722e48325
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-04-13 13:36:05 -0300

    External OAuth2 configuration

--- backend/src/main/java/com/lucasmonteiro/movieflix/config/AuthorizationServerConfig.java
+++ backend/src/main/java/com/lucasmonteiro/movieflix/config/AuthorizationServerConfig.java
@@ -1,6 +1,7 @@
 package com.lucasmonteiro.movieflix.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,6 +16,15 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+	
+	@Value("${security.oauth2.client.client-id}")
+	private String clientId;
+	
+	@Value("${security.oauth2.client.client-secret}")
+	private String clientSecret;
+	
+	@Value("${jwt.duration}")
+	private Integer jwtDuration;
 	
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
@@ -36,11 +46,11 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		clients.inMemory()
-		.withClient("movieflix")
-		.secret(passwordEncoder.encode("movieflix123"))
+		.withClient(clientId)
+		.secret(passwordEncoder.encode(clientSecret))
 		.scopes("read", "write")
 		.authorizedGrantTypes("password")
-		.accessTokenValiditySeconds(86400);
+		.accessTokenValiditySeconds(jwtDuration);
 	}
 
 	@Override
