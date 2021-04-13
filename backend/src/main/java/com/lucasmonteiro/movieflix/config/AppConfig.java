6b77f6ea3da889b13229b71ae33f964c27997da6
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-04-13 13:11:46 -0300

    Implementing OAuth2 authorization server

--- backend/src/main/java/com/lucasmonteiro/movieflix/config/AppConfig.java
+++ backend/src/main/java/com/lucasmonteiro/movieflix/config/AppConfig.java
@@ -3,6 +3,8 @@ package com.lucasmonteiro.movieflix.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 public class AppConfig {
@@ -10,6 +12,18 @@ public class AppConfig {
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+		tokenConverter.setSigningKey("MY-JWT-SECRET");
+		return tokenConverter;
+	}
+
+	@Bean
+	public JwtTokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter());
 	}
 
 }
