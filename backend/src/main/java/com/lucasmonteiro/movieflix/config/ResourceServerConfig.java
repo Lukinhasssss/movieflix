14c60e06c9ed322c6cc78f92adc96fb19516c723
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-04-13 15:19:14 -0300

    Implementing OAuth2 resource server

---
+++ backend/src/main/java/com/lucasmonteiro/movieflix/config/ResourceServerConfig.java
@@ -0,0 +1,44 @@
+package com.lucasmonteiro.movieflix.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+	
+	@Autowired
+	private JwtTokenStore tokenStore;
+	
+	private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**" };
+	
+	private static final String[] CREATE_ACCOUNT = { "/users" };
+	
+	private static final String[] POST_REVIEW = { "/reviews" };
+	
+	private static final String[] VISITOR_AND_MEMBER = { "/movies/**", "reviews/**", "genres/**" };
+	
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+		resources.tokenStore(tokenStore);
+	}
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		
+		http.authorizeRequests()
+		.antMatchers(PUBLIC).permitAll()
+		.antMatchers(HttpMethod.POST, CREATE_ACCOUNT).hasAnyRole("MEMBER")
+		.antMatchers(HttpMethod.POST, POST_REVIEW).hasRole("MEMBER")
+		.antMatchers(HttpMethod.GET, VISITOR_AND_MEMBER).hasAnyRole("VISITOR", "MEMBER")
+		.anyRequest().authenticated();
+		
+	}
+
+}
