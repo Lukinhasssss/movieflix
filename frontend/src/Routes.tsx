b57e6ba65b44e9041cdc3e31834b4d37f5b67444
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-04-16 22:36:02 -0300

    Redirecting from / to /movies when user is logged

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -1,18 +1,26 @@
-import { Route, Router, Switch } from "react-router-dom";
+import { Redirect, Route, Router, Switch } from "react-router-dom";
 import Navbar from "./core/components/Navbar";
 import history from "./core/utils/history";
 import Home from "./pages/Home";
 import Movies from "./pages/Movies";
 import PrivateRoute from "./core/components/Routes/PrivateRoute";
+import { isAuthenticated } from "./core/utils/auth";
 
 const Routes = () => (
   <Router history={ history }>
     <Navbar />
 
     <Switch>
-      <Route path="/" exact>
-        <Home />
-      </Route>
+      <Route
+        path="/"
+        exact
+        render={() => {
+          return (isAuthenticated() ?
+            <Redirect to='/movies' /> :
+            <Home />
+          )
+        }}
+      />
 
       <PrivateRoute path="/movies">
         <Movies />
