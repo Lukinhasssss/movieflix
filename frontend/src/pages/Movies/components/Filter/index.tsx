9ef398a30a48b46b5fa5850694de49a89b13faaa
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-04-19 23:13:54 -0300

    Finish filter

--- frontend/src/pages/Movies/components/Filter/index.tsx
+++ frontend/src/pages/Movies/components/Filter/index.tsx
@@ -1,9 +1,56 @@
+import { useEffect, useState } from 'react'
+import Select from 'react-select'
+import { Genre } from '../../../../core/types/Movie'
+import { makePrivateRequest } from '../../../../core/utils/requests'
 import './styles.scss'
 
-const Filter = () => {
+export type FilterData = {
+  genreId?: number
+}
+
+type Props = {
+  onSearch: (filter: FilterData) => void
+}
+
+const Filter = ({ onSearch }: Props) => {
+  const [isLoadingGenres, setIsLoadingGenres] = useState(false)
+  const [genres, setGenres] = useState<Genre[]>()
+  const [genre, setGenre] = useState<Genre>()
+
+  const handleChangeGenre = (genre: Genre) => {
+    setGenre(genre)
+
+    onSearch({ genreId: genre?.id })
+  }
+
+  useEffect(() => {
+    setIsLoadingGenres(true)
+
+    makePrivateRequest({ url: '/genres' })
+      .then(response => {
+        setGenres(response.data)
+      })
+      .finally(() => {
+        setIsLoadingGenres(false)
+      })
+  }, [])
+
   return (
     <div className="filter-container">
-      Aventura
+      <Select
+        name="genres"
+        key={ `select-${genre?.id}` }
+        value={ genre }
+        isLoading={ isLoadingGenres }
+        options={ genres }
+        getOptionValue={ (option: Genre) => String(option.id) }
+        getOptionLabel={ (option: Genre) => option.name }
+        className="filter-select-container"
+        classNamePrefix="genres-select"
+        placeholder="Filtrar por gênero"
+        isClearable
+        onChange={ value => handleChangeGenre(value as Genre) }
+      />
     </div>
   )
 }
--- frontend/src/pages/Movies/index.tsx
+++ frontend/src/pages/Movies/index.tsx
@@ -1,34 +1,35 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+
+import Filter, { FilterData } from './components/Filter';
+import MovieCard from './components/MovieCard';
 import { MovieResponse } from '../../core/types/Movie';
 import { makePrivateRequest } from '../../core/utils/requests';
-import Filter from './components/Filter';
-import MovieCard from './components/MovieCard';
+
 import './styles.scss';
-
-
 
 const Movies = () => {
   const [moviesResponse, setMoviesResponse] = useState<MovieResponse>()
 
-  const getProducts = () => {
+  const getMovies = useCallback((filter?: FilterData) => {
     const params = {
-      linesPerPage: 16
+      linesPerPage: 8,
+      genreId: filter?.genreId
     }
 
     makePrivateRequest({ url: '/movies', params })
       .then(response => {
         setMoviesResponse(response.data)
       })
-  }
+  }, [])
 
   useEffect(() => {
-    getProducts()
-  }, [])
+    getMovies()
+  }, [getMovies])
 
   return (
     <div className="movies-container">
-      <Filter />
+      <Filter onSearch={ filter => getMovies(filter) } />
 
       <div className="movie-content">
         {moviesResponse?.content.map(movie => (
